f39ced564fb850e1a76539e893faf3ab6e1bc991
nikolaysaitov <47886929+nikolaysaitov@users.noreply.github.com>
2021-09-23 10:34:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let phrases = [
   { text: 'посмотреть скидки на авиабилеты и свалить из страны', image: 'https://code.s3.yandex.net/web-code/procrastinate/2.png' },
   { text: 'разобраться, о чём поют рэперы', image: 'https://code.s3.yandex.net/web-code/procrastinate/3.png' },
   { text: 'Юрий будет Дудь', image: 'https://code.s3.yandex.net/web-code/procrastinate/4.png' },
-  { text: 'расставить книги на полке по цвету', image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png' },
+  { text: 'Пурумпумпум', image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png' },
   { text: 'смотреть как разводят мосты', image: 'https://code.s3.yandex.net/web-code/procrastinate/6.png' },
   { text: 'прочитать новости и ужаснуться от того, что Россия помогает Талибану(запрещенному на территории РФ)', image: 'https://code.s3.yandex.net/web-code/procrastinate/7.png' },
   { text: 'попасть в поток грустных песен и вспомнить все ошибки молодости', image: 'https://code.s3.yandex.net/web-code/procrastinate/8.png' },
   { text: 'посмотреть трейлер сериала и заодно первые 3 сезона', image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png' },
-  { text: 'проверить непрочитанное в Telegram-каналах', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' }
+  { text: 'Румпумпум', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' }
 ];
 
 function getRandomElement(arr) {
